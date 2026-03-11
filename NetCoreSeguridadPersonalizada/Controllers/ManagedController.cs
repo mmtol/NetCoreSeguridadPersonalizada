136cdc8e0b27c13a9dd4665161c69c7abd1d6edb
mmtol <mmigueltolosana@gmail.com>
2026-03-11 09:19:10 +0100

    logout

--- NetCoreSeguridadPersonalizada/Controllers/ManagedController.cs
+++ NetCoreSeguridadPersonalizada/Controllers/ManagedController.cs
@@ -50,5 +50,11 @@ namespace NetCoreSeguridadPersonalizada.Controllers
                 return View();
             }
         }
+
+        public async Task<IActionResult> Logout()
+        {
+            await HttpContext.SignOutAsync(CookieAuthenticationDefaults.AuthenticationScheme);
+            return RedirectToAction("Index", "Home");
+        }
     }
 }
